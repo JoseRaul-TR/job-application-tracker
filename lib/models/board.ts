7f0f297807d73c847a036ebebfc84848a5175846
José_Raúl Tenza_Ramírez <jraultr@gmail.com>
2026-06-01 09:36:53 +0200

    First commit after completing Youtube tutorial from PedroTech

---
+++ lib/models/board.ts
@@ -0,0 +1,39 @@
+// lib/models/board.ts
+
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IBoard extends Document {
+  name: string;
+  userId: string;
+  columns: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+// Board -> Columns -> JobbApplications
+
+const BoardSchema = new Schema<IBoard>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    columns: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Column",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.models.Board ||
+  mongoose.model<IBoard>("Board", BoardSchema);
---
+++ lib/models/index.ts
@@ -0,0 +1,9 @@
+// lib/models/index.ts
+
+import "./board";
+import "./column";
+import "./job-application";
+
+export { default as Board } from "./board";
+export { default as Column } from "./column";
+export { default as JobApplication } from "./job-application";
